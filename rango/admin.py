3faaa03a5a9803c4223e425703332c098c3e43fc
Segun Bamgbose <segun4099@gmail.com>
2017-08-23 03:26:47 -0700

    Successfully add the slug field to my models.

--- rango/admin.py
+++ rango/admin.py
@@ -3,11 +3,13 @@ from rango.models import Category, Page
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
+    fields = ['name',]
     list_display = ('name',)
     list_filter = ['views']
 
 
 class PageAdmin(admin.ModelAdmin):
+    fields = ['title', 'category', 'url',]
     list_display = ('title', 'category', 'url',)
     list_filter = ['category']
 

5233a948b36e7a6a1ff100e989015ce158fd39cb
Segun Bamgbose <segun4099@gmail.com>
2017-08-23 03:14:24 -0700

    About to add the slug field to my models. Errors usually occurs here

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rango.models import Category, Page
 
 # Create your views here.
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake"}
-    return render(request, 'rango/index.html', context=context_dict)
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    return render(request, 'rango/index.html', context_dict)
 
 
 def about(request):

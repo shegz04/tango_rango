813508b691d7af68225699a738ad48d6e056a0e6
Segun Bamgbose <segun4099@gmail.com>
2017-08-23 03:00:32 -0700

    Added Models and populate script

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from rango.models import Category, Page
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ['views']
+
+
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url',)
+    list_filter = ['category']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page, PageAdmin)
